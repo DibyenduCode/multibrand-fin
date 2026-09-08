90460e9e262ec8de844f605e996afd540068e9b9
Dibyendu <khaskeldibyendu22@gmail.com>
2026-09-08 18:14:06 +0530

    Update PWA app short name to Biswas Fin

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -10,7 +10,7 @@
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="apple-mobile-web-app-title" content="MB Finance">
+    <meta name="apple-mobile-web-app-title" content="Biswas Fin">
     <link rel="apple-touch-icon" href="<?= BASE_URL ?>/pwa-icon.php?size=192">
     <!-- Google Fonts: Inter -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
